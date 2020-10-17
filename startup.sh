7d2cab75c42be7f1b81b1386760cf81494c849ca
Ben Jackson <b@benjackson.email>
2020-10-17 15:16:35 +1100

    add startup script

---
+++ startup.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+if [ ! -f config.yml ]; then
+    echo "Creating config file"
+    cp example-config.yml config.yml
+fi
+if [ ! -f docker-compose.fake-powerwall.yml ] && [ "$1" == "fakepowerwall" ]; then
+    echo "Setting up fake powerwall docker-compose override file"
+    cp example-docker-compose.fake-powerwall.yml docker-compose.fake-powerwall.yml
+fi
+
+if [ "$1" == "fakepowerwall" ]; then
+    echo "Attempting to start controller with fake powerwall support"
+    docker-compose -f docker-compose.yml -f docker-compose.fake-powerwall.yml up -d
+else
+    echo "Attempting to start controller"
+    docker-compose up -d
+fi
