c2b97f38fdb3484e3beaa15fb6ad8d9d31892536
Ben Jackson <b@benjackson.email>
2020-10-18 20:03:25 +1100

    update scripts to use new docker-compose way

--- startup.sh
+++ startup.sh
@@ -1,18 +1,22 @@
 #!/bin/bash
 
-if [ ! -f config.yml ]; then
+if [ ! -f config.yml ]
+then
     echo "Creating config file"
     cp example-config.yml config.yml
 fi
-if [ ! -f docker-compose.fake-powerwall.yml ] && [ "$1" == "fakepowerwall" ]; then
-    echo "Setting up fake powerwall docker-compose override file"
-    cp example-docker-compose.fake-powerwall.yml docker-compose.fake-powerwall.yml
+
+if [ "$1" == "fakepowerwall" ]
+then
+    echo "Setting up fake powerwall docker-compose file"
+    cp example-docker-compose.fake-powerwall.yml docker-compose.yml
+    echo "Setting up fake powerwall environment variables"
+    echo "INVERTER_HOST=${INVERTER_HOST}" > .env.fakepowerwall
+    echo "INVERTER_TYPE=${INVERTER_TYPE}" >> .env.fakepowerwall
+else
+    echo "Setting up docker-compose file"
+    cp example-docker-compose.yml docker-compose.yml
 fi
 
-if [ "$1" == "fakepowerwall" ]; then
-    echo "Attempting to start controller with fake powerwall support"
-    docker-compose -f docker-compose.yml -f docker-compose.fake-powerwall.yml up -d --remove-orphans
-else
-    echo "Attempting to start controller"
-    docker-compose up -d --remove-orphans
-fi
+echo "Attempting to start controller"
+docker-compose up -d --remove-orphans
