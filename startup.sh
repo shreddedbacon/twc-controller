331faf9c675b4eb7509da081d04c4ed67b0db6d4
Ben Jackson <b@benjackson.email>
2020-10-18 20:26:17 +1100

    add opts to startup.sh and fix user-data with the changes

--- startup.sh
+++ startup.sh
@@ -1,12 +1,42 @@
 #!/bin/bash
 
+usage() {
+  echo "Usage: ./startup.sh -p fakepowerwall -i http://192.168.1.50 -t fronius"
+  echo "WARNING: specifying -p fakepowerwall without options -i or -t will result in errors"
+  echo "Options:"
+  echo "  -p fakepowerwall"
+  echo "  -i <inverter_host>"
+  echo "  -t <inverter_type>"
+  exit 1
+}
+
+if [[ ! $@ =~ ^\-.+ ]]
+then
+  usage
+fi
+
+while getopts ":p:i:t:h:" opt; do
+  case ${opt} in
+    p ) # process option p
+      FAKEPOWERWALL=$OPTARG;;
+    t ) # process option d
+      INVERTER_TYPE=$OPTARG;;
+    i ) # process option s
+      INVERTER_HOST=$OPTARG;;
+    h )
+      usage;;
+    *)
+      usage;;
+  esac
+done
+
 if [ ! -f config.yml ]
 then
     echo "Creating config file"
     cp example-config.yml config.yml
 fi
 
-if [ "$1" == "fakepowerwall" ]
+if [ "$FAKEPOWERWALL" == "fakepowerwall" ]
 then
     echo "Setting up fake powerwall docker-compose file"
     cp example-docker-compose.fake-powerwall.yml docker-compose.yml
