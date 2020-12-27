9197271e7479dd82295878d0579fb7e25d76ddbc
Ben Jackson <b@benjackson.email>
2020-12-28 10:04:32 +1100

    add build version info

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 VERSION=${1}
 PUSH=${2}
 if [ "${VERSION}" != "" ]; then
-  docker build -t --build-arg TWC_BUILD_VERSION=${VERSION} shreddedbacon/twc-controller:arm32v6-rpi-${VERSION} .
+  docker build -t shreddedbacon/twc-controller:arm32v6-rpi-${VERSION} --build-arg TWC_BUILD_VERSION=${VERSION} .
   if [ "$PUSH" == "push" ]; then
     docker push shreddedbacon/twc-controller:arm32v6-rpi-${VERSION}
   fi
