82b934de372158dabb8faf6e9936aef7b49b5105
Ben Jackson <b@benjackson.email>
2020-12-28 10:13:07 +1100

    add build version info

--- internal/controller/primary.go
+++ internal/controller/primary.go
@@ -52,7 +52,7 @@ type TWCPrimary struct {
 	LEDController          *ledStrip       `yaml:"-"`
 	LEDValues              *LEDValues      `yaml:"-"`
 	LEDCharging            bool            `yaml:"-"`
-	twcBuildVersion        string          `yaml:"-"`
+	TWCBuildVersion        string          `yaml:"-"`
 }
 
 // TeslaAPIUser holds the API user
@@ -112,7 +112,7 @@ func NewPrimary(primary TWCPrimary, port *serial.Port) (*TWCPrimary, error) {
 	}
 
 	// get the env POWERWALL_HOST as an override if defined
-	primary.twcBuildVersion = getEnv("TWC_BUILD_VERSION", "")
+	primary.TWCBuildVersion = getEnv("TWC_BUILD_VERSION", "")
 	primary.Powerwall = getEnv("POWERWALL_HOST", primary.Powerwall)
 	primary.sign = []byte{0x77}
 	primary.timeLastTx = int64(0)
