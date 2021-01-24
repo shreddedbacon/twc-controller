e2b05afdd1b08f5115cddabf453d39aba53c7b14
Ben Jackson <b@benjackson.email>
2021-01-24 16:35:59 +1100

    use new v2 messaging which only supports 1 twc

--- internal/controller/polling.go
+++ internal/controller/polling.go
@@ -9,8 +9,7 @@ import (
 // PollSecondaryKWH polls the secondary TWCs for their usage statistics
 func (p *TWCPrimary) PollSecondaryKWH() (int64, error) {
 	for _, twc := range p.knownTWCs {
-		fmt.Println(twc)
-		if p.DebugLevel >= 1 {
+		if p.DebugLevel >= 15 {
 			log.Println(log2JSONString(LogData{
 				Type:     "INFO",
 				Source:   "polling",
@@ -89,23 +88,23 @@ func (p *TWCPrimary) PollModel() (int64, error) {
 // PollVINStart polls the secondary TWCs for the current VIN
 func (p *TWCPrimary) PollVINStart() (int64, error) {
 	for _, twc := range p.knownTWCs {
-		if twc.ReportedState != 0 {
-			if p.DebugLevel >= 15 {
-				log.Println(log2JSONString(LogData{
-					Type:     "INFO",
-					Source:   "polling",
-					Sender:   fmt.Sprintf("%x", p.ID),
-					Receiver: fmt.Sprintf("%x", twc.TWCID),
-					Message:  "Poll Secondary for VIN start",
-				}))
-			}
-			// msg := append(append(append([]byte{0xFB, 0xEE}, p.ID...), twc.TWCID...), []byte{0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00}...)
-			msg := append(append([]byte{0xFB, 0xEE}, p.ID...), twc.TWCID...)
-			padBytes(&msg)
-			_, _ = SendMessage(p.DebugLevel, p.port, msg)
-			// p.ReadMessage()
-			time.Sleep(100 * time.Millisecond)
+		// if twc.ReportedState != 0 {
+		if p.DebugLevel >= 15 {
+			log.Println(log2JSONString(LogData{
+				Type:     "INFO",
+				Source:   "polling",
+				Sender:   fmt.Sprintf("%x", p.ID),
+				Receiver: fmt.Sprintf("%x", twc.TWCID),
+				Message:  "Poll Secondary for VIN start",
+			}))
 		}
+		// msg := append(append(append([]byte{0xFB, 0xEE}, p.ID...), twc.TWCID...), []byte{0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00}...)
+		msg := append(append([]byte{0xFB, 0xEE}, p.ID...), twc.TWCID...)
+		padBytes(&msg)
+		_, _ = SendMessage(p.DebugLevel, p.port, msg)
+		// p.ReadMessage()
+		time.Sleep(100 * time.Millisecond)
+		// }
 	}
 	return time.Now().UTC().Unix(), nil
 }
@@ -113,23 +112,23 @@ func (p *TWCPrimary) PollVINStart() (int64, error) {
 // PollVINMiddle polls the secondary TWCs for the current VIN
 func (p *TWCPrimary) PollVINMiddle() (int64, error) {
 	for _, twc := range p.knownTWCs {
-		if twc.ReportedState != 0 {
-			if p.DebugLevel >= 15 {
-				log.Println(log2JSONString(LogData{
-					Type:     "INFO",
-					Source:   "polling",
-					Sender:   fmt.Sprintf("%x", p.ID),
-					Receiver: fmt.Sprintf("%x", twc.TWCID),
-					Message:  "Poll Secondary for VIN middle",
-				}))
-			}
-			// msg := append(append(append([]byte{0xFB, 0xEF}, p.ID...), twc.TWCID...), []byte{0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00}...)
-			msg := append(append([]byte{0xFB, 0xEF}, p.ID...), twc.TWCID...)
-			padBytes(&msg)
-			_, _ = SendMessage(p.DebugLevel, p.port, msg)
-			// p.ReadMessage()
-			time.Sleep(100 * time.Millisecond)
+		// if twc.ReportedState != 0 {
+		if p.DebugLevel >= 15 {
+			log.Println(log2JSONString(LogData{
+				Type:     "INFO",
+				Source:   "polling",
+				Sender:   fmt.Sprintf("%x", p.ID),
+				Receiver: fmt.Sprintf("%x", twc.TWCID),
+				Message:  "Poll Secondary for VIN middle",
+			}))
 		}
+		// msg := append(append(append([]byte{0xFB, 0xEF}, p.ID...), twc.TWCID...), []byte{0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00}...)
+		msg := append(append([]byte{0xFB, 0xEF}, p.ID...), twc.TWCID...)
+		padBytes(&msg)
+		_, _ = SendMessage(p.DebugLevel, p.port, msg)
+		// p.ReadMessage()
+		time.Sleep(100 * time.Millisecond)
+		// }
 	}
 	return time.Now().UTC().Unix(), nil
 }
@@ -137,23 +136,23 @@ func (p *TWCPrimary) PollVINMiddle() (int64, error) {
 // PollVINEnd polls the secondary TWCs for the current VIN
 func (p *TWCPrimary) PollVINEnd() (int64, error) {
 	for _, twc := range p.knownTWCs {
-		if twc.ReportedState != 0 {
-			if p.DebugLevel >= 15 {
-				log.Println(log2JSONString(LogData{
-					Type:     "INFO",
-					Source:   "polling",
-					Sender:   fmt.Sprintf("%x", p.ID),
-					Receiver: fmt.Sprintf("%x", twc.TWCID),
-					Message:  "Poll Secondary for VIN end",
-				}))
-			}
-			// msg := append(append(append([]byte{0xFB, 0xF1}, p.ID...), twc.TWCID...), []byte{0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00}...)
-			msg := append(append([]byte{0xFB, 0xF1}, p.ID...), twc.TWCID...)
-			padBytes(&msg)
-			_, _ = SendMessage(p.DebugLevel, p.port, msg)
-			// p.ReadMessage()
-			time.Sleep(100 * time.Millisecond)
+		// if twc.ReportedState != 0 {
+		if p.DebugLevel >= 15 {
+			log.Println(log2JSONString(LogData{
+				Type:     "INFO",
+				Source:   "polling",
+				Sender:   fmt.Sprintf("%x", p.ID),
+				Receiver: fmt.Sprintf("%x", twc.TWCID),
+				Message:  "Poll Secondary for VIN end",
+			}))
 		}
+		// msg := append(append(append([]byte{0xFB, 0xF1}, p.ID...), twc.TWCID...), []byte{0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00}...)
+		msg := append(append([]byte{0xFB, 0xF1}, p.ID...), twc.TWCID...)
+		padBytes(&msg)
+		_, _ = SendMessage(p.DebugLevel, p.port, msg)
+		// p.ReadMessage()
+		time.Sleep(100 * time.Millisecond)
+		// }
 	}
 	return time.Now().UTC().Unix(), nil
 }
@@ -161,23 +160,23 @@ func (p *TWCPrimary) PollVINEnd() (int64, error) {
 // PollPlugState polls the secondary TWCs for their plug state
 func (p *TWCPrimary) PollPlugState() (int64, error) {
 	for _, twc := range p.knownTWCs {
-		if twc.ReportedState != 0 {
-			if p.DebugLevel >= 15 {
-				log.Println(log2JSONString(LogData{
-					Type:     "INFO",
-					Source:   "polling",
-					Sender:   fmt.Sprintf("%x", p.ID),
-					Receiver: fmt.Sprintf("%x", twc.TWCID),
-					Message:  "Poll Secondary for plug state",
-				}))
-			}
-			// msg := append(append(append([]byte{0xFB, 0xB4}, p.ID...), twc.TWCID...), []byte{0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00}...)
-			msg := append(append([]byte{0xFB, 0xB4}, p.ID...), twc.TWCID...)
-			padBytes(&msg)
-			_, _ = SendMessage(p.DebugLevel, p.port, msg)
-			// p.ReadMessage()
-			time.Sleep(100 * time.Millisecond)
+		// if twc.ReportedState != 0 {
+		if p.DebugLevel >= 15 {
+			log.Println(log2JSONString(LogData{
+				Type:     "INFO",
+				Source:   "polling",
+				Sender:   fmt.Sprintf("%x", p.ID),
+				Receiver: fmt.Sprintf("%x", twc.TWCID),
+				Message:  "Poll Secondary for plug state",
+			}))
 		}
+		// msg := append(append(append([]byte{0xFB, 0xB4}, p.ID...), twc.TWCID...), []byte{0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00}...)
+		msg := append(append([]byte{0xFB, 0xB4}, p.ID...), twc.TWCID...)
+		padBytes(&msg)
+		_, _ = SendMessage(p.DebugLevel, p.port, msg)
+		// p.ReadMessage()
+		time.Sleep(100 * time.Millisecond)
+		// }
 	}
 	return time.Now().UTC().Unix(), nil
 }
